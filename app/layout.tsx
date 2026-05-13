a4da536eafe559da434daa2a7d455d04683c3964
Takumi Takahashi <takumitakahashi@TakuminoMacBook-Pro.local>
2026-05-13 22:37:43 +0900

    Add AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" className="h-full">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2913908713051662" crossOrigin="anonymous" />
+      </head>
       <body className="h-full bg-gray-950">{children}</body>
     </html>
   )
